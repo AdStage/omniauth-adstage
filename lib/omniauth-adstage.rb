6656b1bac459e43f0f2c63fbbc639ddad077330b
Jason Wu <jason@adstage.io>
2015-12-14 15:31:33 -0800

    Register parser for HAL content type.

--- lib/omniauth-adstage.rb
+++ lib/omniauth-adstage.rb
@@ -1,3 +1,8 @@
-require "omniauth-adstage/version"
+require 'omniauth-adstage/version'
 require 'omniauth/strategies/adstage'
 
+# Ensure OAuth2::Response supports parsing HAL.
+require 'oauth2/response'
+::OAuth2::Response.register_parser(:hal_json, ['application/hal+json']) do |body|
+  MultiJson.load(body) rescue body
+end
